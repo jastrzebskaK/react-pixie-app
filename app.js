90f9f3d710dfed30f6b077c38026a7d705184e46
Karolina Jastrzebska <karolina.jastrzebska2709@gmail.com>
2021-03-22 18:46:23 +0100

    Add new files

---
+++ app.js
@@ -0,0 +1,52 @@
+class App extends React.Component {
+
+   state={
+      oman:'',
+      omany : ['Wygrasz na loterii ','Dobry czas na miłość ','Dobry czas na naukę Reacta '],
+      value: '',
+    }
+
+    handleDraw=() => {
+       
+        
+        const index = Math.round(Math.random()*(this.state.omany.length-1))
+        this.setState({
+           oman: this.state.omany[index] 
+               })
+ 
+    }
+
+    handleAdd = () => {
+        const omany = [...this.state.omany]
+        omany.push(this.state.value)
+        this.setState({
+            omany,
+            value: ''
+        })
+        alert(`Wróżba została dodana`)
+    }
+
+    handleInputChange=(event) => {
+        this.setState({
+            value:event.target.value
+        })
+
+    }
+
+    render() {
+        
+        return(
+            <React.Fragment>
+            <button className="losuj" onClick={this.handleDraw}>Wylosuj  wróżbę na dziś</button>
+            <br/>
+            <input type="text" value={this.state.value} onChange={this.handleInputChange}></input>
+            <button className="dodaj" onClick={this.handleAdd}>Dodaj własną wróżbę</button>
+            <br/>
+            <br/>
+            {this.state.oman ? <label >{this.state.oman}</label> : null}
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
